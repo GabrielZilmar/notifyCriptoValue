b8cda1d90fa40505717d76051c509729defd6713
Gabriel Zilmar <gabrielzilmar@hotmail.com>
2022-03-27 00:24:46 -0300

    ci: reset tests

--- src/modules/whatsAppBot/routes/get.test.ts
+++
@@ -1,8 +0,0 @@
-import EndpointBuilder from "../../httpServer/utils/EndpointBuilder";
-import { HttpMethod } from "../../../types/HttpMethod";
-
-export default EndpointBuilder.new("/test")
-  .setHttpMethod(HttpMethod.GET)
-  .setHandler(async (req, res) => {
-    res.status(200).send("Teste");
-  });
